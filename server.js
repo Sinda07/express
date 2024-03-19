b872e82baaddeb652e030c76741504f6b775b724
Sinda07 <guesmisinda123@gmail.com>
2024-03-19 12:32:16 +0100

    api

--- server.js
+++ server.js
@@ -1,118 +1,63 @@
 const express = require("express");
-const connect = require("./config/connectDb");
-connect();
-const PORT = 4000;
 const mongoose = require("mongoose");
-const {
-  createPerson,
-  createManyPeople,
-  findPeopleByName,
-  findOneByFood,
-  findPersonById,
-  findEditThenSave,
-  findAndUpdate,
-  removeById,
-  removeManyPeople,
-} = require("./models/person");
+require("dotenv").config();
+const User = require("./models/User");
 const app = express();
-app.use(express.json);
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://guesmisinda123:Sindafifi123@cluster0.8hyag9a.mongodb.net/person"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.post("/users", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+  });
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.put("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deletedUser = await User.findByIdAndRemove(id);
+    res.json(deletedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// Create Person Model
-const Person = mongoose.model("Person", personSchema);
-
-// Create and Save a Record of a Model
-const createPerson = (name, age, favoriteFoods, done) => {
-  const person = new Person({ name, age, favoriteFoods });
-  person.save(function (err, data) {
-    if (err) return console.error(err);
-    done(null, data);
-  });
-};
-
-// Create Many Records with model.create()
-const createManyPeople = (arrayOfPeople, done) => {
-  Person.create(arrayOfPeople, function (err, data) {
-    if (err) return console.error(err);
-    done(null, data);
-  });
-};
-
-// Use model.find() to Search Your Database
-const findPeopleByName = (personName, done) => {
-  Person.find({ name: personName }, function (err, data) {
-    if (err) return console.error(err);
-    done(null, data);
-  });
-};
-
-// Use model.findOne() to Return a Single Matching Document from Your Database
-const findOneByFood = (food, done) => {
-  Person.findOne({ favoriteFoods: food }, function (err, data) {
-    if (err) return console.error(err);
-    done(null, data);
-  });
-};
-
-// Use model.findById() to Search Your Database By _id
-const findPersonById = (personId, done) => {
-  Person.findById(personId, function (err, data) {
-    if (err) return console.error(err);
-    done(null, data);
-  });
-};
-
-// Perform Classic Updates by Running Find, Edit, then Save
-const findEditThenSave = (personId, done) => {
-  Person.findById(personId, function (err, person) {
-    if (err) return console.error(err);
-    person.favoriteFoods.push("hamburger");
-    person.save(function (err, data) {
-      if (err) return console.error(err);
-      done(null, data);
-    });
-  });
-};
-
-// Perform New Updates on a Document Using model.findOneAndUpdate()
-const findAndUpdate = (personName, done) => {
-  Person.findOneAndUpdate(
-    { name: personName },
-    { age: 20 },
-    { new: true },
-    function (err, data) {
-      if (err) return console.error(err);
-      done(null, data);
-    }
-  );
-};
-
-// Delete One Document Using model.findByIdAndRemove
-const removeById = (personId, done) => {
-  Person.findByIdAndRemove(personId, function (err, data) {
-    if (err) return console.error(err);
-    done(null, data);
-  });
-};
-
-//  Delete Many Documents with model.remove()
-const removeManyPeople = (done) => {
-  Person.remove({ name: "Mary" }, function (err, data) {
-    if (err) return console.error(err);
-    done(null, data);
-  });
-};
-
-module.exports = {
-  createPerson,
-  createManyPeople,
-  findPeopleByName,
-  findOneByFood,
-  findPersonById,
-  findEditThenSave,
-  findAndUpdate,
-  removeById,
-  removeManyPeople,
-};
